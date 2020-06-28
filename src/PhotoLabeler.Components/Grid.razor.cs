a4c987fc7905cc9e93560da897830a92999593cb
dani herrera <ctrl.alt.d@gmail.com>
2020-06-28 20:47:13 +0200

    refactor pendent focus

---
+++ src/PhotoLabeler.Components/Grid.razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace PhotoLabeler.Components
+{
+    public partial class Grid
+    {
+        [Inject] 
+		public IJSRuntime jsRuntime { get; set; }
+
+        [Parameter]
+        public Entities.Grid Model { get; set; }
+
+        [Parameter]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        private bool focusOnItemAfterRender = false;
+       
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (focusOnItemAfterRender)
+            {
+                await jsRuntime.InvokeVoidAsync("jsInteropFunctions.focusSelectedItemInsideContainer", this.Id);
+            }
+        }
+
+
+    }
+}
