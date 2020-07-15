f7132366655c351bd968aafb60980f8b0debae6f
dani herrera <ctrl.alt.d@gmail.com>
2020-07-15 08:08:07 +0200

    1.- Avoid distorted

--- src/PhotoLabeler.PhotoStorageReader/Implementations/PhotoReaderBase64.cs
+++ src/PhotoLabeler.PhotoStorageReader/Implementations/PhotoReaderBase64.cs
@@ -11,6 +11,7 @@ namespace PhotoLabeler.PhotoStorageReader.Implementations
 	{
 		public string GetImgSrc(string path)
 		{
+			const int uiSize = 100;
 			var postedFileExtension = Path.GetExtension(path);
 			var isImage = (string.Equals(postedFileExtension, ".jpg", StringComparison.OrdinalIgnoreCase)
 							|| string.Equals(postedFileExtension, ".png", StringComparison.OrdinalIgnoreCase)
@@ -21,9 +22,16 @@ namespace PhotoLabeler.PhotoStorageReader.Implementations
 			{
 				using (var image = Image.Load(path))
 				{
+					var (x, y) = (image.Width, image.Height);
+					var minSide = Math.Min(x,y);
+					var (resizedX, resizedY) = (uiSize*x/minSide, uiSize*y/minSide);
+					var centerArea = new Rectangle( (resizedX-uiSize)/2, (resizedY-uiSize)/2, uiSize, uiSize);
+
 					image.Mutate(x => x
-						.Resize(100, 100)
+						.Resize(resizedX, resizedY)
+						.Crop(centerArea)
 						);
+
 					return image.ToBase64String(JpegFormat.Instance);
 				}
 			}
